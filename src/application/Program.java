4b6a039131c072d432c715bf4ab3ec8153fc2c4b
Daniel Matos <dan.souza.matos@gmail.com>
2023-01-10 16:31:41 -0300

    Acrescentado tratativa para MismatchException

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import model.entities.Reservation;
@@ -41,6 +42,8 @@ public class Program {
 			System.out.println("Invalid date format");
 		} catch (DomainException e) {
 			System.out.println("Error in reservation: " + e.getMessage());
+		} catch (InputMismatchException e) {
+			System.out.println("Invalid data type");
 		} catch (RuntimeException e) {
 			System.out.println("Unexpected error");
 		}
